d2dc2a553a6f97ed14531c947e209140c3aacf01
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-07-28 22:56:06 +0530

    Create Prime_Check.java

---
+++ Prime_Check.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        int n,c=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                c++;
+            }
+        }
+        if(c==2){
+            System.out.println("Prime");
+        }
+        else {
+            System.out.println("Not Prime");
+        }
+    }
+}
