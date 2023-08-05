998af7b33c7c8d36a0e2b0094900945e5a05d99f
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-05 23:24:11 +0530

    Create Basics.java

---
+++ Day1/Basics.java
@@ -0,0 +1,18 @@
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void main(String[] args) {
+        
+        //System.out.println("Hello World!");
+        
+        System.out.println("Hi, Welcome to AlgoPrepSession");
+        System.out.print(" World");
+        System.out.println(19+"Hell");
+        System.out.println(10+9);
+        System.out.println(10*8+7*7-78);
+        
+        int temp=20;
+        System.out.println(temp);
+        temp=40;
+        System.out.println(temp);
+    }
+}
