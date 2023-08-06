819f2142ba65bffa40b4b8ff6d7b1d0484e56587
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-06 15:37:10 +0530

    Create Double_float_if.java

---
+++ Day3/Double_float_if.java
@@ -0,0 +1,21 @@
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void main(String[] args) {
+        double d=4.4;
+        float f=4.8f;
+        System.out.println(d);
+        System.out.println(f);
+        
+        double d1=f;
+        System.out.println(d1);
+        
+        int a=100000;
+        int b=100000;
+        long c=(long)(a*b);
+        System.out.println(c);
+        
+        System.out.println(16/3);
+        System.out.println(35%9);
+        System.out.println(25.0/3);
+    }
+}
