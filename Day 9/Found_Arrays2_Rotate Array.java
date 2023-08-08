1d9bc472a555a7d31580ca9be82c782147cbf1d2
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-08 19:07:35 +0530

    Create Found_Arrays2_Rotate Array.java

---
+++ Day 9/Found_Arrays2_Rotate Array.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        if(k>n){
+            k=k%n;
+        }
+        Reverse(arr,0,n-1);
+        Reverse(arr,0,k-1);
+        Reverse(arr,k,n-1);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void Reverse(int arr[],int s,int e){
+        int sp=s;
+        int ep=e;
+        while(sp<ep){
+            int temp=arr[sp];
+            arr[sp]=arr[ep];
+            arr[ep]=temp;
+            sp++;
+            ep--;
+        }
+    }
+}
