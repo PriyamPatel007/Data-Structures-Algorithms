d6ae1f839ef3c4af87413338662948f27c39fbb5
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-13 11:17:58 +0530

    Create AlgoPrep_Found_IsPrime.java

---
+++ Day4/AlgoPrep_Found_IsPrime.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if(count==2){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Not Prime");
+        }
+    }
+}
