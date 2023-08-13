bde96e4f31fd43f724e7bcefb63d2c0a3500d747
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-13 12:14:11 +0530

    Create Pattern1LineJava.java

---
+++ Day6/Pattern1LineJava.java
@@ -0,0 +1,10 @@
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.print("*");
+        }
+    }
+}
