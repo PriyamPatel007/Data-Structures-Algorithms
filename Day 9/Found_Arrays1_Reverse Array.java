8baff55ce87f86e34217ea547e81ed27259ccd4e
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-14 16:09:21 +0530

    Create Found_Arrays1_Reverse Array.java

---
+++ Day 9/Found_Arrays1_Reverse Array.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();   
+        }
+        Reverse(n,arr);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void Reverse(int n, int arr[]){
+        int s=0;
+        int e=n-1;
+        while(s<e){
+            int temp=arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++;
+            e--;
+        }
+    }
+}
