f449fd9d7b122bd79f22537937eab493cfb1fbc4
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-15 20:22:58 +0530

    Create Found_Arrays2_Max Difference 4_HW.java

---
+++ Day 10/Found_Arrays2_Max Difference 4_HW.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int cs=0;
+        int indDiff=0;
+        int MaxDiff=arr[0];
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                cs=Math.abs(arr[i]-arr[j]);
+                indDiff=cs+Math.abs(i-j);
+                MaxDiff=Math.max(MaxDiff,indDiff);
+            }
+        }
+        System.out.println(MaxDiff);
+    }
+}
