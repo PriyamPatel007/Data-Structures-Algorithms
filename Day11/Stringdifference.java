61f859aa689fe4a47ba0160c2a196193532010da
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-18 13:08:30 +0530

    Create Stringdifference.java

---
+++ Day11/Stringdifference.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        String st=sc.nextLine();
+        String s="";
+        int diff=0;
+        int n=st.length();
+        for(int i=0;i<n-1;i++){
+            s+=st.charAt(i);
+            diff=st.charAt(i+1)-st.charAt(i);
+            s+=diff;
+        }
+        s+=st.charAt(st.length()-1);
+        System.out.println(s);
+    }
+}
