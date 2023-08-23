b1e021685ce51af62abdd57c31fae1b785e6f8e8
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-23 18:04:22 +0530

    Create PrintIncreasing.java

---
+++ recursionDay18/PrintIncreasing.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Dec(n);
+    }
+    public static void Dec(int n){
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
+
+        Dec(n-1);
+                System.out.println(n);
+        return;
+    }
+}
