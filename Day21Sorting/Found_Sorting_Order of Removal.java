fe1ae258421d0fc09534dad3157cf51aebe71c14
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-29 23:20:15 +0530

    Create Found_Sorting_Order of Removal.java

---
+++ Day21Sorting/Found_Sorting_Order of Removal.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int temp=0,ans=0;
+        for(int i=0;i<n;i++){
+            temp=arr[i]*(n-i);
+            ans+=temp;
+        }
+        System.out.println(ans);
+    }
+}
