e44a540118159b35463e1be5365376ddbc4ba9bf
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-08-31 23:19:03 +0530

    Update 540. Single Element in a Sorted Array.java

--- Day24Searching/540. Single Element in a Sorted Array.java
+++ Day24Searching/540. Single Element in a Sorted Array.java
@@ -3,6 +3,16 @@ class Solution {
         int n=nums.length;
         int lo=2;
         int hi=n-3;
+        if(nums.length == 1){
+            return nums[0];
+        }
+  if(nums[0] != nums[1]){
+            return nums[0];
+        }
+        if(nums[nums.length-1] != nums[nums.length - 2]){
+            return nums[nums.length-1];
+        }
+
         while(lo<=hi){
             if(nums[0]!=nums[1]){
                 return nums[0];
@@ -10,7 +20,7 @@ class Solution {
             if(nums[n-1]!=nums[n-2]){
                 return nums[n-1];
             }
-            int mid=(lo+hi)/2;
+            int mid=lo + (hi-lo)/2;
             if((nums[mid]!=nums[mid-1]) && (nums[mid]!=nums[mid+1])){
                 return nums[mid];
             }
