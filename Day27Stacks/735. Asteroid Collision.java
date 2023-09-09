427a0678a1d4a4ccf18158265298922df1956c64
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-09-10 00:29:14 +0530

    Create 735. Asteroid Collision.java

---
+++ Day27Stacks/735. Asteroid Collision.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        int n=asteroids.length;
+        Stack<Integer> st=new Stack<>();
+        int i=0;
+while(i < n){
+if(asteroids[i] < 0){
+if(st.size()==0 || st.peek()<0){
+st.push(asteroids[i]);
+i++;
+}
+else if(st.peek() == Math.abs(asteroids[i])){
+st.pop();
+i++;
+}
+else if(st.peek()< Math.abs(asteroids[i])){
+st.pop();
+}
+else if(st.peek() > Math.abs(asteroids[i])){
+i++;
+}
+}else{
+st.push(asteroids[i]);
+i++;
+}
+}
+
+        int ans[]=new int[st.size()];
+        for(int j=ans.length-1;j>=0;j--){
+            ans[j]=st.pop();
+        }
+        return ans;
+    }
+}
