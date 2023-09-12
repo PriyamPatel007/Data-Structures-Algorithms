d76db4bf10e048cf3aae0d12007d6b698188cd9e
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-09-12 18:36:21 +0530

    Create Found_Queue_Even Palindrome Kth Number.java

---
+++ Day28Queue/Found_Queue_Even Palindrome Kth Number.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        Queue<String> q=new LinkedList<>();
+        q.add("11");
+        q.add("22");
+        String ans="";
+        for(int i=1;i<=k;i++){
+            String temp=q.remove();
+            ans=temp;
+            String left=temp.substring(0,temp.length()/2);
+            String right=temp.substring(temp.length()/2,temp.length());
+            q.add(left+"11"+right);
+            q.add(left+"22"+right);
+        }
+        System.out.println(ans);
+    }
+}
