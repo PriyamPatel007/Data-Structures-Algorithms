d04de0a2e88d53b5ba66a5afa619ba7efe5f6765
PRIYAM PATEL <43856835+PriyamPatel007@users.noreply.github.com>
2023-09-17 18:44:58 +0530

    Create 509. Fibonacci Number.java

---
+++ DynamicProgrammingDay32/509. Fibonacci Number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n,dp);
+}
+public static int helper(int n,int dp[]){
+    if(n==0 || n==1){
+        return n;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    int a=helper(n-1,dp);
+    int b=helper(n-2,dp);
+    dp[n]=a+b;
+    return a+b;
+}
+}
